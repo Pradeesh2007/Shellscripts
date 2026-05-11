ecbad8321bd0eee6388d67fa8fa7827bb1071dc4
Pradeesh kumar <155274729+Pradeesh2007@users.noreply.github.com>
2026-05-11 14:20:57 +0530

    Update keycopyscipt.sh

--- keycopyscipt.sh
+++ keycopyscipt.sh
@@ -2,8 +2,9 @@
 echo "enter the key name only 'minato'"
 ssh-keygen 
 echo "copy the public key to the server"
-ssh-copy-id -i ./minato ansible@$1
-ssh-copy-id -i ./minato ansible@$2
-ssh-copy-id -i ./minato ansible@$3
+ssh-copy-id -i ./minato.pub ansible@$1
+ssh-copy-id -i ./minato.pub ansible@$2
+ssh-copy-id -i ./minato.pub ansible@$3
+echo "private_key_file = ./minato" >> ansible.cfg
 echo "test the connection"
-ansible all -m ping -u ansible
+ansible all -m ping -u ansible --private-key ./minato
