d5ab1cbae27a3aa8b0d811835dddac08f4cf4798
Pradeesh kumar <155274729+Pradeesh2007@users.noreply.github.com>
2026-05-11 13:20:14 +0530

    Update usercreate.sh

--- usercreate.sh
+++ usercreate.sh
@@ -3,5 +3,6 @@
 sudo adduser ansible
 sudo passwd ansible
 sudo sh -c 'echo "ansible ALL=(ALL:ALL) ALL" >> /etc/sudoers'   
+sudo nano /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
 echo "user created succesfully..................."
