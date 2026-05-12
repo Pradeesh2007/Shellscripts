1957738420633d735321f9733ac8929e2ea4b570
Pradeesh kumar <155274729+Pradeesh2007@users.noreply.github.com>
2026-05-12 16:42:53 +0530

    Update usercreate.sh

--- usercreate.sh
+++ usercreate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+echo "creating user ansible"
+echo "change no to yes when passwdauthention file apper"
 sudo adduser ansible
 sudo passwd ansible
 sudo sh -c 'echo "ansible ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers'   
 sudo nano /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+echo "Restarting ssh deamon"
 sudo systemctl restart ssh
 sudo systemctl restart sshd
-echo "user created succesfully..................."
+echo "user created succesfully..............."
