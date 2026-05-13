acdfd9461d2c0c0cc1cc8273c32acf3fb142b63c
Pradeesh kumar <155274729+Pradeesh2007@users.noreply.github.com>
2026-05-13 09:38:54 +0530

    Modify user creation script for ansible user
    Replaced 'adduser' with 'useradd' for user creation and updated SSH configuration to enable password authentication.

--- usercreate.sh
+++ usercreate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "creating user ansible"
 echo "change no to yes when passwdauthention file apper"
-sudo adduser ansible
+sudo useradd ansible
 sudo passwd ansible
 sudo sh -c 'echo "ansible ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers'   
-sudo nano /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+sudo sh -c 'echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/60-cloudimg-settings.conf' 
 echo "Restarting ssh deamon"
 sudo systemctl restart ssh
 sudo systemctl restart sshd
